557d89a910684b9d62e90e9e9b033a614c70f87a
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-10 22:31:44 +0200

    My second commit Module4.1 Task5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,12 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-        switch (year % 4) {
-            case 0 :
-                System.out.println("leap");
-                break;
-            default:
-                System.out.println("not leap");
+        if ((year % 100 == 0) && (year % 400 == 0)){
+            System.out.println("leap");
+        } else if(year % 4 == 0) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
         }
     }
 }
